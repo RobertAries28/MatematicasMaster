32ef50ef530724a4686cc327a59427a8970ee98f
RobertAries28 <61357261+RobertAries28@users.noreply.github.com>
2020-03-17 01:25:26 -0500

    correo

--- correo.php
+++ correo.php
@@ -2,14 +2,14 @@
   
   if (isset($_POST['enviar'])){
       if(!empty($_POST['nombre']) && !empty($_POST['apellidos']) && !empty($_POST['correo']) && !empty($_POST['mensaje'])){
-          $nombre=$_POST['nombre'];
-          $apellidos=$_POST['apellidos'];
-          $correo=$_POST['correo'];
-          $mensaje=$_POST['mensaje'];
+          $nombre= $_POST['nombre'];
+          $apellidos= $_POST['apellidos'];
+          $correo= $_POST['correo'];
+          $mensaje= $_POST['mensaje'];
           $header= "Form: robert_15_95@hotmail.com" . "\r\n";
           $header.= "Replay-To: robert_15_95@hotmail.com" . "\r\n";
           $header.= "X-Mailer: PHP/". phpversion();
-          $mail($nombre,$apellidos,$correo,$mensaje,$header);
+          $mail= @mail($correo,$apellidos,$nombre,$mensaje,$header);
           if ($mail){
               echo "<h4>mail enviado exitosamente </h4>";
           }
